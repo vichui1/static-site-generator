97ba4f76a63e2859282b18027c18855292df224c
Victor Hui <huivictor01@gmail.com>
2025-06-15 11:17:42 +0200

    docs for github pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,45 +1,57 @@
 from markdown_blocks import markdown_to_html_node
 import os
-    
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for entry in os.listdir(dir_path_content):
         entry_path = os.path.join(dir_path_content, entry)
         entry_dest_path = os.path.join(dest_dir_path, entry)
-        if os.path.isfile(entry_path) and entry_path.endswith('.md'):
-            entry_dest_path = entry_dest_path.removesuffix('.md') + '.html'
-            generate_page(entry_path, template_path, entry_dest_path)
+        if os.path.isfile(entry_path) and entry_path.endswith(".md"):
+            entry_dest_path = entry_dest_path.removesuffix(".md") + ".html"
+            generate_page(entry_path, template_path, entry_dest_path, basepath)
         else:
-            generate_pages_recursive(entry_path, template_path, entry_dest_path)     
+            generate_pages_recursive(
+                entry_path, template_path, entry_dest_path, basepath
+            )
 
-def generate_page(from_path, template_path, dest_path):
+
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     markdown_content = read_file(from_path)
     template_content = read_file(template_path)
     html_string = markdown_to_html_node(markdown_content).to_html()
     title = extract_title(markdown_content)
-    html_content = template_content.replace('{{ Title }}', title).replace('{{ Content }}', html_string)
+    html_content = (
+        template_content.replace("{{ Title }}", title)
+        .replace("{{ Content }}", html_string)
+        .replace('href="/', f'href="{basepath}')
+        .replace('src="/', f'src="{basepath}')
+    )
     dir_path = os.path.dirname(dest_path)
     os.makedirs(dir_path, exist_ok=True)
     write_file(dest_path, html_content)
-    
+
+
 def read_file(file_path):
     try:
-        with open(file_path, 'r', encoding='utf-8') as file:
+        with open(file_path, "r", encoding="utf-8") as file:
             return file.read()
     except FileNotFoundError as e:
         print(f"Error: The file '{file_path}' was not found.")
         raise e
     except Exception as e:
         print(f"An error occured: {e}")
-        raise e    
+        raise e
+
 
 def write_file(file_path, content):
-    with open(file_path, 'w', encoding='utf-8') as file:
+    with open(file_path, "w", encoding="utf-8") as file:
         file.write(content)
+
 
 def extract_title(markdown):
     for line in markdown.splitlines():
         line = line.strip()
-        if line.startswith('# '):
-            return line.lstrip('# ')
+        if line.startswith("# "):
+            return line.lstrip("# ")
     raise ValueError("markdown doesn't contain a h1 heading")
--- src/main.py
+++ src/main.py
@@ -2,16 +2,21 @@ from textnode import TextNode, TextType
 from file_utils import copy_to_dir
 from generate_page import generate_pages_recursive
 import os
+import sys
 
-def main():
+def main(args):
+    if len(args) > 1:
+        basepath = args[1]
+    else:
+        basepath = '/'
     current_dir = os.getcwd()
     static_path = os.path.join(current_dir, "static")
-    public_path = os.path.join(current_dir, "public")
+    docs_path = os.path.join(current_dir, "docs")
 
-    copy_to_dir(static_path, public_path)
+    copy_to_dir(static_path, docs_path)
     content_path = os.path.join(current_dir, "content")
     template_path = os.path.join(current_dir, "template.html")
-    public_path = os.path.join(current_dir, 'public')
-    generate_pages_recursive(content_path, template_path, public_path)
+    generate_pages_recursive(content_path, template_path, docs_path, basepath)
 
-main()
+if __name__ == '__main__':
+    main(sys.argv)
